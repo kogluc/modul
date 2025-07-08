85a91e141342c649dc3226e992373497f95c5be9
kogluc <muhcanerkiziloglu@gmail.com>
2025-07-08 12:50:46 +0300

    Update ana.js

--- js/ana.js
+++ js/ana.js
@@ -1,21 +1,25 @@
-window.komutuIsle = async function(komut) {
+// Global fonksiyon olarak tanımlıyoruz (window içine)
+window.komutuIsle = async function (komut) {
   switch (komut) {
-    case 'koyu':
+    case 'koyu': {
       const temaModul = await import('./modul/temaDegistirici.js');
       temaModul.temayiUygula?.("dark");
       break;
-      
-    case 'acik':
+    }
+
+    case 'acik': {
       const temaModul = await import('./modul/temaDegistirici.js');
-      temaModul.temayiUygula?.("acik");
+      temaModul.temayiUygula?.("light");
       break;
-      
-    case 'veri':
+    }
+
+    case 'veri': {
       const veriModul = await import('./modul/veriYukleyici.js');
-      veriModul.verileriYukle();
+      veriModul.verileriYukle?.();
       break;
+    }
 
     default:
       alert("Bilinmeyen komut: " + komut);
   }
-}
+};
