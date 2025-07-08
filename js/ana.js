f8c91047652604f197bc779df9f59901f67f95b5
kogluc <muhcanerkiziloglu@gmail.com>
2025-07-08 12:34:50 +0300

    Update ana.js

--- js/ana.js
+++ js/ana.js
@@ -1,16 +1,7 @@
-window.komutuIsle = async function(komut) {
-  switch (komut) {
-    case 'tema':
-      const temaModul = await import('./modul/temaDegistirici.js');
-      temaModul.temayiUygula?.("dark");
-      break;
+import { komutuIsle } from './modul/komutIsleyici.js';
 
-    case 'veri':
-      const veriModul = await import('./modul/veriYukleyici.js');
-      veriModul.verileriYukle();
-      break;
-
-    default:
-      alert("Bilinmeyen komut: " + komut);
-  }
-}
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById("komutBtn")?.addEventListener("click", () => {
+    komutuIsle("tema");
+  });
+});
