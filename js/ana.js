b40fa0a2b6b801bc9bb2e94c45ce5234cbc2e24e
kogluc <muhcanerkiziloglu@gmail.com>
2025-07-08 13:42:55 +0300

    Update ana.js

--- js/ana.js
+++ js/ana.js
@@ -1,8 +1,13 @@
 window.komutuIsle = async function(komut) {
   switch (komut) {
-    case 'tema':
+    case 'dark':
       const temaModul = await import('./modul/temaDegistirici.js');
       temaModul.temayiUygula?.("dark");
+      break;
+
+    case "light":
+      const temaModul = await import('./temaDegistirici.js');
+      temaModul.temayiUygula("light");
       break;
 
     case 'veri':
