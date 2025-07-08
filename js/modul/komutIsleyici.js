939633fcf353cf245cdc70b5dcaf6f525e2cce0b
kogluc <muhcanerkiziloglu@gmail.com>
2025-07-08 12:33:28 +0300

    Update komutIsleyici.js

--- js/modul/komutIsleyici.js
+++ js/modul/komutIsleyici.js
@@ -1,7 +1,17 @@
-window.komutIsleyiciKur = function () {
-  return {
-    komutuIsle: function (komut) {
-      alert("Komut alındı: " + komut);
-    }
-  };
-};
+export async function komutuIsle(komut) {
+  switch (komut) {
+    case "tema":
+      const temaModul = await import('./temaDegistirici.js');
+      temaModul.temayiUygula("dark");
+      break;
+
+    case "veri":
+      const veriModul = await import('./veriYukleyici.js');
+      veriModul.verileriYukle();
+      break;
+
+    default:
+      console.warn("Tanımsız komut:", komut);
+      alert("Tanımsız komut: " + komut);
+  }
+}
